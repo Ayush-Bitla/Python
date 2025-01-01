c99308797db3f124ef68b4026769eefd76e17d9a
Ayush Bitla <161107424+Ayush-Bitla@users.noreply.github.com>
2025-01-01 23:12:13 +0530

    Add files via upload

---
+++ Day1/meaning.py
@@ -0,0 +1,10 @@
+meaning = 40
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not Today')
+
+# ternary opearator
+print('Right on!') if meaning > 10 else print('Not Today')
